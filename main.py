31a8aba2359382792da00ecb5dcdbc3b8f70ee84
Karl Stratos <me@karlstratos.com>
2019-12-10 18:16:35 -0500

    .

---
+++ main.py
@@ -0,0 +1,207 @@
+# Author: Karl Stratos (me@karlstratos.com)
+import argparse
+import copy
+import inspect
+import math
+import matplotlib.pyplot as plt
+import numpy as np
+import random
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import util
+
+from collections import OrderedDict
+
+
+def control_weights(args, models):
+
+    def init_weights(m):
+        if hasattr(m, 'weight') and hasattr(m.weight, 'uniform_') and \
+           args.init > 0.0:
+            torch.nn.init.uniform_(m.weight, a=-args.init, b=args.init)
+
+    for name in models:
+        models[name].apply(init_weights)
+
+    # Exactly match estimators for didactice purposes.
+    if args.carry == 0:  # MINE(0) == DV
+        models['mine'].fXY = copy.deepcopy(models['dv'].fXY)
+
+    if args.alpha == 1:  # INTERPOL(1, *) == CPC
+        models['interpol'].fXY = copy.deepcopy(models['cpc'].fXY)
+        models['cpc'].transpose = True
+
+    if args.alpha == 0 and args.a == 'e':  # INTERPOL(0, e) == NWJ
+        models['interpol'].fXY = copy.deepcopy(models['nwj'].fXY)
+
+
+def main(args):
+    print(args)
+    random.seed(args.seed)
+    torch.manual_seed(args.seed)
+    pXY = util.CorrelatedStandardNormals(args.dim, args.rho)
+
+    models = {
+        'dv': util.SingleSampleEstimator(args.dim, args.hidden, args.layers,
+                                         'dv'),
+        'mine': util.MINE(args.dim, args.hidden, args.layers,
+                          carry_rate=args.carry),
+        'nwj': util.SingleSampleEstimator(args.dim, args.hidden, args.layers,
+                                          'nwj'),
+        'nwjjs': util.NWJJS(args.dim, args.hidden, args.layers),
+        'cpc': util.CPC(args.dim, args.hidden, args.layers),
+        'interpol': util.Interpolated(args.dim, args.hidden, args.layers,
+                                      args.a, args.alpha),
+        'doe': util.DoE(args.dim, args.hidden, args.layers, 'gauss'),
+        'doe_l': util.DoE(args.dim, args.hidden, args.layers, 'logistic')
+    }
+    control_weights(args, models)
+
+    optims = {name: torch.optim.Adam(models[name].parameters(), lr=args.lr)
+              for name in models}
+    train_MIs = {name: [] for name in models}
+
+    for step in range(1, args.steps + 1):
+        X, Y = pXY.draw_samples(args.N)
+        XY_package = torch.cat([X.repeat_interleave(X.size(0), 0),
+                                Y.repeat(Y.size(0), 1)], dim=1)
+        L = {}
+        for name in models:
+            optims[name].zero_grad()
+
+            L[name] = models[name](X, Y, XY_package)
+            L[name].backward()
+            train_MIs[name].append(-L[name].item())
+
+            nn.utils.clip_grad_norm_(models[name].parameters(), args.clip)
+            optims[name].step()
+
+        print('step {:4d} | '.format(step), end='')
+        for name in L:
+            print('{:s}: {:6.2f} | '.format(name, -L[name]), end='')
+        print('ln N: {:.2f} | I(X,Y): {:.2f}'.format(math.log(args.N), pXY.I()))
+
+    # Final evaluation
+    M = 10 * args.N
+    X, Y = pXY.draw_samples(M)
+    XY_package = torch.cat([X.repeat_interleave(M, 0), Y.repeat(M, 1)], dim=1)
+    test_MI = {}
+    for name in models:
+        models[name].eval()
+        test_MI[name] = -models[name](X, Y, XY_package)
+
+    print('-'*150)
+    print('Estimates on {:d} samples | '.format(M), end='')
+    for name in test_MI:
+        print('{:s}: {:6.2f} | '.format(name, test_MI[name]), end='')
+    print('ln({:d}): {:.2f} | I(X,Y): {:.2f}'.format(M, math.log(M), pXY.I()))
+
+    return test_MI, train_MIs, pXY.I()
+
+
+def meta_main(args):
+
+    hypers = OrderedDict({
+        'hidden': [64, 128],
+        'layers': [1],
+        'lr': [0.01, 0.003, 0.001, 0.0003],
+        'init': [0.0, 0.1, 0.05],
+        'clip': [1, 5, 10],
+        'carry': [0.99, 0.9, 0.5],
+        'alpha': [0.01, 0.5, 0.99],
+        'a': ['e'],
+        'seed': list(range(100000))
+    })
+
+    best_test_MI = {}
+    best_train_MIs = {}
+    bestargs = {}
+    for run_number in range(1, args.nruns + 1):
+        if args.nruns > 1:
+            print('RUN NUMBER: %d' % (run_number))
+            for hyp, choices in hypers.items():
+                choice = choices[torch.randint(len(choices), (1,)).item()]
+                assert hasattr(args, hyp)
+                args.__dict__[hyp] = choice
+
+        test_MI, train_MIs, mi = main(args)
+        for name in test_MI:
+            if run_number == 1:
+                best_test_MI[name] = test_MI[name]
+                best_train_MIs[name] = train_MIs[name]
+                bestargs[name] = copy.deepcopy(args)
+            else:
+                if abs(mi - test_MI[name]) < abs(mi - best_test_MI[name]):
+                    best_test_MI[name] = test_MI[name]
+                    best_train_MIs[name] = train_MIs[name]
+                    bestargs[name] = copy.deepcopy(args)
+
+    plt.figure(figsize=(12,5))
+    x = range(1, args.steps + 1)
+    plt.plot(x, best_train_MIs['doe'], "-r", label='DoE (Gauss)',
+             linewidth=0.5)
+    plt.plot(x, best_train_MIs['doe_l'], color='tab:orange',
+             label='DoE (Logistic)', linewidth=0.5)
+    plt.plot(x, best_train_MIs['mine'], "-g", label='MINE', linewidth=0.5)
+    plt.plot(x, best_train_MIs['cpc'], "-b", label='CPC', linewidth=0.5)
+    plt.plot(x, best_train_MIs['nwj'], "-y", label='NWJ', linewidth=0.5)
+    plt.plot(x, best_train_MIs['nwjjs'], "-m", label='NWJ (JS)', linewidth=0.5)
+    plt.plot(x, best_train_MIs['interpol'], "-c", label='CPC+NWJ',
+             linewidth=0.5)
+    plt.plot(x, [mi for _ in range(args.steps)], '-k', label='I(X,Y)')
+    plt.plot(x, [math.log(args.N) for _ in range(args.steps)],
+             linestyle='dashed', color='0.5', label='ln N')
+    plt.legend(loc="center left", bbox_to_anchor=(1, 0.5))
+    plt.ylim(-1.5, max(max(best_train_MIs['doe']), mi) + 5)
+    plt.xlim(1, args.steps)
+    plt.savefig('best_train.pdf', bbox_inches='tight')
+
+    print('-'*150)
+    print('Best test estimates on {:d} samples'.format(10 * args.N))
+    print('-'*150)
+    for name in best_test_MI:
+        print('{:10s}: {:6.2f} \t\t {:s}'.format(name, best_test_MI[name],
+                                                 str(bestargs[name])))
+    print('-'*150)
+    print('ln({:d}): {:.2f}'.format(10 * args.N, math.log(10 * args.N)))
+    print('I(X,Y): {:.2f}'.format(mi))
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--N', type=int, default=64,
+                        help='number of samples [%(default)d]')
+    parser.add_argument('--rho', type=float, default=0.5,
+                        help='correlation coefficient [%(default)g]')
+    parser.add_argument('--dim', type=int, default=20,
+                        help='number of dimensions [%(default)d]')
+    parser.add_argument('--hidden', type=int, default=100,
+                        help='dimension of hidden states [%(default)d]')
+    parser.add_argument('--layers', type=int, default=1,
+                        help='number of hidden layers [%(default)d]')
+    parser.add_argument('--lr', type=float, default=0.01,
+                        help='initial learning rate [%(default)g]')
+    parser.add_argument('--init', type=float, default=0.0,
+                        help='param init (default if 0) [%(default)g]')
+    parser.add_argument('--clip', type=float, default=1,
+                        help='gradient clipping [%(default)g]')
+    parser.add_argument('--steps', type=int, default=1000, metavar='T',
+                        help='number of training steps [%(default)d]')
+    parser.add_argument('--carry', type=float, default=0.99,
+                        help='EMA carry rate [%(default)g]')
+    parser.add_argument('--alpha', type=float, default=1.0,
+                        help='interpolation weight (on CPC term) [%(default)g]')
+    parser.add_argument('--a', type=str, default='e', choices=['e', 'ff', 'lp'],
+                        help='score function in TUBA, INTERPOL [%(default)s]')
+    parser.add_argument('--nruns', type=int, default=1,
+                        help='number of random runs (not random if set to 1) '
+                        '[%(default)d]')
+    parser.add_argument('--seed', type=int, default=42,
+                        help='random seed [%(default)d]')
+    parser.add_argument('--cuda', action='store_true',
+                        help='use CUDA?')
+
+    args = parser.parse_args()
+    meta_main(args)
---
+++ util.py
@@ -0,0 +1,318 @@
+# Author: Karl Stratos (me@karlstratos.com)
+import math
+import torch
+import torch.nn as nn
+
+from torch.distributions.multivariate_normal import MultivariateNormal
+
+
+class DoE(nn.Module):
+
+    def __init__(self, dim, hidden, layers, pdf):
+        super(DoE, self).__init__()
+        self.qY = PDF(dim, pdf)
+        self.qY_X = ConditionalPDF(dim, hidden, layers, pdf)
+
+    def forward(self, X, Y, XY_package):
+        hY = self.qY(Y)
+        hY_X = self.qY_X(Y, X)
+
+        loss = hY + hY_X
+        mi_loss = hY_X - hY
+        return (mi_loss - loss).detach() + loss
+
+
+class ConditionalPDF(nn.Module):
+
+    def __init__(self, dim, hidden, layers, pdf):
+        super(ConditionalPDF, self).__init__()
+        assert pdf in {'gauss', 'logistic'}
+        self.dim = dim
+        self.pdf = pdf
+        self.X2Y = FF(dim, hidden, 2 * dim, layers)
+
+    def forward(self, Y, X):
+        mu, ln_var = torch.split(self.X2Y(X), self.dim, dim=1)
+        cross_entropy = compute_negative_ln_prob(Y, mu, ln_var, self.pdf)
+        return cross_entropy
+
+
+class PDF(nn.Module):
+
+    def __init__(self, dim, pdf):
+        super(PDF, self).__init__()
+        assert pdf in {'gauss', 'logistic'}
+        self.dim = dim
+        self.pdf = pdf
+        self.mu = nn.Embedding(1, self.dim)
+        self.ln_var = nn.Embedding(1, self.dim)  # ln(s) in logistic
+
+    def forward(self, Y):
+        cross_entropy = compute_negative_ln_prob(Y, self.mu.weight,
+                                                 self.ln_var.weight, self.pdf)
+        return cross_entropy
+
+
+class NWJJS(nn.Module):
+
+    def __init__(self, dim, hidden, layers):
+        super(NWJJS, self).__init__()
+        self.fXY = FF(2 * dim, hidden, 1, layers)
+        self.lsig = nn.LogSigmoid()
+        self.sp = nn.Softplus()
+
+    def forward(self, X, Y, XY_package):
+        N = int(math.sqrt(XY_package.size(0)))
+        infs = torch.tensor([float('inf')] * N)
+
+        S = self.fXY(XY_package).view(N, N)
+
+        accept = self.lsig(S).diag().mean()
+        reject = self.lsig(-S)
+        reject = (reject - reject.diag().diag()).sum() / N / (N - 1)
+        js = -(accept + reject)
+
+        nwj = (S - infs.diag()).exp().sum() / N / (N - 1) / math.e - \
+              S.diag().mean()
+
+        return (nwj - js).detach() + js
+
+
+class Interpolated(nn.Module):
+
+    def __init__(self, dim, hidden, layers, aY_type, alpha):
+        super(Interpolated, self).__init__()
+        assert alpha >= 0 and alpha <= 1
+        self.fXY = FF(2 * dim, hidden, 1, layers)
+        self.ln_aY = get_ln_score_function(dim, hidden, layers, aY_type)
+        self.ln_alpha = math.log(alpha) if alpha > 0 else float('-inf')
+        self.ln_one_minus_alpha = math.log(1 - alpha) if alpha < 1 \
+                                  else float('-inf')
+
+    def forward(self, X, Y, XY_package):
+        N = int(math.sqrt(XY_package.size(0)))
+
+        S = self.fXY(XY_package).view(N, N)
+        ln_aY = self.ln_aY(Y)
+
+        joint = self.get_joint_term(S, ln_aY, N)
+        loo = self.get_loo_term(S, ln_aY, N)
+
+        return loo - joint - 1
+
+    def get_loo_term(self, S, ln_aY, N):
+        infs = torch.tensor([float('inf')] * N)
+        ln_sumexp_Y_loo = (torch.logsumexp(S - infs.diag(), 0)
+                           - math.log(N - 1)).view(N, 1)
+        ln_interpol_loo = torch.cat([self.ln_alpha + ln_sumexp_Y_loo,
+                                     self.ln_one_minus_alpha + ln_aY],
+                                    dim=1)
+        ln_interpol_loo = torch.logsumexp(ln_interpol_loo, 1)
+
+        exp_loo = torch.logsumexp(S - ln_interpol_loo - infs.diag(), 0)
+        exp_loo = (exp_loo.exp() / (N - 1)).mean()
+        return exp_loo
+
+    def get_joint_term(self, S, ln_aY, N):
+        ln_sumexp_Y = (torch.logsumexp(S, 0) - math.log(N)).view(N, 1)
+        ln_interpol = torch.cat([self.ln_alpha + ln_sumexp_Y,
+                                 self.ln_one_minus_alpha + ln_aY],
+                                dim=1)
+        ln_interpol = torch.logsumexp(ln_interpol, 1)
+        joint = (S - ln_interpol).diag().mean()
+        return joint
+
+
+class CPC(nn.Module):
+
+    def __init__(self, dim, hidden, layers):
+        super(CPC, self).__init__()
+        self.fXY = FF(2 * dim, hidden, 1, layers)
+        self.ce = nn.CrossEntropyLoss()
+        self.transpose = False
+
+    def forward(self, X, Y, XY_package):
+        N = int(math.sqrt(XY_package.size(0)))
+        infs = torch.tensor([float('inf')] * N)
+
+        S = self.fXY(XY_package).view(N, N)
+        if self.transpose:
+            S = S.t()
+        loss = self.ce(S, torch.tensor([i for i in range(N)]))
+
+        return loss - math.log(N)
+
+
+class SingleSampleEstimator(nn.Module):
+
+    def __init__(self, dim, hidden, layers, estimator_type):
+        super(SingleSampleEstimator, self).__init__()
+        self.estimator_type = estimator_type
+        self.fXY = FF(2 * dim, hidden, 1, layers)
+
+    def forward(self, X, Y, XY_package):
+        N = int(math.sqrt(XY_package.size(0)))
+        infs = torch.tensor([float('inf')] * N)
+
+        S = self.fXY(XY_package).view(N, N)
+        joint = S.diag().mean()
+        exp_marginal = (S - infs.diag()).exp().sum() / N / (N - 1)
+        return self.squash(exp_marginal) - joint
+
+    def squash(self, exp_marginal):
+        if self.estimator_type == 'dv':
+            return exp_marginal.log()
+        elif self.estimator_type == 'nwj':
+            return exp_marginal / math.e
+        else:
+            raise ValueError('Unknown estimator: %s' % (self.estimator_type))
+
+
+class MINE(nn.Module):
+
+    def __init__(self, dim, hidden, layers, carry_rate=0.99):
+        super(MINE, self).__init__()
+        self.fXY = FF(2 * dim, hidden, 1, layers)
+        self.carry_rate = carry_rate
+        self.ema = None
+
+    def forward(self, X, Y, XY_package):
+        N = int(math.sqrt(XY_package.size(0)))
+        infs = torch.tensor([float('inf')] * N)
+
+        S = self.fXY(XY_package).view(N, N)
+        joint = S.diag().mean()
+        exp_marginal = (S - infs.diag()).exp().sum() / N / (N - 1)
+
+        self.ema = exp_marginal.detach() if self.ema is None else \
+                   self.carry_rate * self.ema + \
+                   (1 - self.carry_rate) * exp_marginal.detach()
+
+        mine_loss = (1 / self.ema) * exp_marginal - joint
+        dv_loss = self.ema.log() - joint
+
+        return (dv_loss - mine_loss).detach() + mine_loss
+
+
+class TUBA(nn.Module):
+
+    def __init__(self, dim, hidden, layers, aX_type):
+        super(TUBA, self).__init__()
+        self.fXY = FF(2 * dim, hidden, 1, layers)
+        self.aX_type = get_ln_score_function(dim, hidden, layers, aX_type)
+
+    def forward(self, X, Y, XY_package):
+        N = int(math.sqrt(XY_package.size(0)))
+        infs = torch.tensor([float('inf')] * N)
+
+        S = self.fXY(XY_package).view(N, N) - self.ln_aX(X)
+        joint = S.diag().mean()
+        exp_marginal = (S - infs.diag()).exp().sum() / N / (N - 1)
+        return exp_marginal - joint - 1
+
+
+class LnConstant(nn.Module):
+
+    def __init__(self, c):
+        super(LnConstant, self).__init__()
+        self.c = c
+
+    def forward(self, X):
+        return torch.tensor([math.log(self.c)] * X.size(0)).view(X.size(0), 1)
+
+
+class LnStandardNormal(nn.Module):
+
+    def __init__(self, dim):
+        super(LnStandardNormal, self).__init__()
+        self.pdf = MultivariateNormal(torch.zeros(dim), torch.eye(dim))
+
+    def forward(self, X):
+        return self.pdf.log_prob(X).view(X.size(0), 1)
+
+
+class FF(nn.Module):
+
+    def __init__(self, dim_input, dim_hidden, dim_output, num_layers,
+                 activation='tanh', dropout_rate=0, layer_norm=False,
+                 residual_connection=False):
+        super(FF, self).__init__()
+        assert (not residual_connection) or (dim_hidden == dim_input)
+        self.residual_connection = residual_connection
+
+        self.stack = nn.ModuleList()
+        for l in range(num_layers):
+            layer = []
+
+            if layer_norm:
+                layer.append(nn.LayerNorm(dim_input if l == 0 else dim_hidden))
+
+            layer.append(nn.Linear(dim_input if l == 0 else dim_hidden,
+                                   dim_hidden))
+            layer.append({'tanh': nn.Tanh(), 'relu': nn.ReLU()}[activation])
+            layer.append(nn.Dropout(dropout_rate))
+
+            self.stack.append(nn.Sequential(*layer))
+
+        self.out = nn.Linear(dim_input if num_layers < 1 else dim_hidden,
+                             dim_output)
+
+    def forward(self, x):
+        for layer in self.stack:
+            x = x + layer(x) if self.residual_connection else layer(x)
+        return self.out(x)
+
+
+class CorrelatedStandardNormals(object):
+
+    def __init__(self, dim, rho):
+        assert abs(rho) <= 1
+        self.dim = dim
+        self.rho = rho
+        self.pdf = MultivariateNormal(torch.zeros(dim), torch.eye(dim))
+
+    def I(self):
+        num_nats = - self.dim / 2 * math.log(1 - math.pow(self.rho, 2)) \
+                   if abs(self.rho) != 1.0 else float('inf')
+        return num_nats
+
+    def hY(self):
+        return 0.5 * self.dim * math.log(2 * math.pi)
+
+    def draw_samples(self, num_samples):
+        X, ep = torch.split(self.pdf.sample((2 * num_samples,)), num_samples)
+        Y = self.rho * X + math.sqrt(1 - math.pow(self.rho, 2)) * ep
+        return X, Y
+
+
+def compute_negative_ln_prob(Y, mu, ln_var, pdf):
+    var = ln_var.exp()
+
+    if pdf == 'gauss':
+        negative_ln_prob = 0.5 * ((Y - mu) ** 2 / var).sum(1).mean() + \
+                           0.5 * Y.size(1) * math.log(2 * math.pi) + \
+                           0.5 * ln_var.sum(1).mean()
+
+    elif pdf == 'logistic':
+        whitened = (Y - mu) / var
+        adjust = torch.logsumexp(torch.stack([torch.zeros(Y.size()),
+                                              -whitened]), 0)
+        negative_ln_prob = whitened.sum(1).mean() + \
+                           2 * adjust.sum(1).mean() + \
+                           ln_var.sum(1).mean()
+
+    else:
+        raise ValueError('Unknown PDF: %s' % (pdf))
+
+    return negative_ln_prob
+
+
+def get_ln_score_function(dim, hidden, layers, function_type):
+        if function_type == 'e':
+            return LnConstant(math.e)
+        elif function_type == 'ff':
+            return FF(dim, hidden, 1, layers)
+        elif function_type == 'lp':
+            return LnStandardNormal(dim)
+        else:
+            raise ValueError('Unknown function type: %s' % (function_type))
